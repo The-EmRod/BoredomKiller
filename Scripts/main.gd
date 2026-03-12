ca70f3dc5118571eca5f42689c0bef79279d2fc6
The-EmRod <michael.rodriguezcolon05@gmail.com>
2026-03-12 00:31:56 -0500

    I CAN USE A TIMER
    HOLY SHEIST FELLAS I CAN USE A TIMER LET'S FREAKING GO
    Next step is to create an array that will hold all of my images I'll be using so that I may call upon them randomly to be displayed.

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,6 +1,7 @@
 extends Node2D
 #Can be called to display megaman
 @onready var megaman = $megaman
+@onready var surfers = $SubwaySurfersA
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,8 +25,13 @@ func _ready() -> void:
 #This function will call and alter the timer to display random
 #images or videos
 func _process(_delta: float) -> void:
-	var timeStamp : int = 300
+	var timeStamp : int = 2000
 	var currentTime : int = Time.get_ticks_msec()
 	
 	if(currentTime <= timeStamp):
+		megaman.hide()
+	else:
+		megaman.show()
+	
+	if currentTime >= (timeStamp*2):
 		megaman.hide()
